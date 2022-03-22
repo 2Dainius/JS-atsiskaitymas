b43b04e3a82f9a049819b96452f009ab01797b31
2Dainius <dainius.drgs@gmail.com>
2022-03-22 12:19:41 +0200

    Class/objects

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,22 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie{
+    constructor(title, director, budget){
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive(){
+        return this.budget > 300000000
+    }
+}
+
+const movie1 = new Movie('Pirates of the Caribbean: On Stranger Tides', 'Rob Marshall', '379000000')
+console.log(movie1)
+console.log(movie1.wasExpensive())
+
+const movie2 = new Movie('The Blair Witch Project', 'Daniel Myrick', '35000')
+console.log(movie2)
+console.log(movie2.wasExpensive())
