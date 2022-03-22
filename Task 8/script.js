cb9400ef9cfaee65f63dc32f33697c0751cf6aec
2Dainius <dainius.drgs@gmail.com>
2022-03-22 12:59:09 +0200

    Calculator

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,24 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator{
+    sum(num1, num2){
+        return num1 + num2;
+    }
+    sub(num1, num2){
+        return num1 - num2;
+    }
+    multi(num1, num2){
+        return num1 * num2;
+    }
+    div(num1, num2){
+        return num1 / num2;
+    }
+}
+
+const example = new Calculator()
+console.log(example.sum(15, 78))
+console.log(example.sub(100, 23))
+console.log(example.multi(15, 10))
+console.log(example.div(72, 9))
